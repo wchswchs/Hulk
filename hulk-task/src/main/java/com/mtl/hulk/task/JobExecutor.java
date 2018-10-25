042663b57cc41d93a06ac599b86d2c60f26165b3
wangchangsheng <wangchangsheng@juqitech.com>
2018-10-25 19:20:26 +0800

    修改pom

--- hulk-task/src/main/java/com/mtl/hulk/task/JobExecutor.java
+++ hulk-task/src/main/java/com/mtl/hulk/task/JobExecutor.java
@@ -1,6 +1,5 @@
 package com.mtl.hulk.task;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,7 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 @ComponentScan(basePackages = {"com.mtl.demo.service.service", "com.mtl.demo.serviceB.service", "com.mtl.demo.serviceC.service", "com.mtl.hulk"}/*, basePackageClasses = {ApplicationContextUtils.class}*/)
 @EnableFeignClients("com.mtl.demo.service.feign")
-@PropertySource({"file:/opt/job/hulk_job_global.properties", "file:/opt/hulk/hulk.properties"})
+@PropertySource({"file:/opt/hulk/hulk_global.properties", "file:/opt/hulk/hulk.properties"})
 public class JobExecutor {
     public static void main(final String[] args) {
         SpringApplication.run(JobExecutor.class, args);
